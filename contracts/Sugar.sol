b66d6991e03c0cc9d272388516e1d501621f566d
Pozzitron <the.vaho1337@gmail.com>
2022-04-03 19:17:28 +0300

    fixed typo than -> then

--- contracts/Sugar.sol
+++ contracts/Sugar.sol
@@ -8,10 +8,10 @@ import "./SugarBlock.sol";
 /** 
  * Sugar is token with ERC20 interface.
  * http://www.bolshoyvopros.ru/questions/3264373-skolko-krupinok-sahara-v-odnom-kologramme-sahara.html
- * let in 1 kilogram of sugar with cristall mass of 0.0005 gramm contains: 1000/0.0005 = 2_000_000 pieces
- * let 2_000_000 SGR represents 1 kilogramm of sugar. Than decimals of SGR equal 6
+ * let in 1 kilogram of sugar with cristall mass of 0.0005 gramm contains: 1000/0.0005 = 2_000_000 pieces 
+ * let 2_000_000 SGR represents 1 kilogramm of sugar. Then decimals of SGR equal 6
  * https://pronakip.com/skolko-kilogramm-saxara-v-meshke/
- * let one sugar block contains 50 kilogramm of sugar. Than in one block 50 * (2 * 10**6) = 100 * (10 ** 6) 100_000_000 SGR
+ * let one sugar block contains 50 kilogramm of sugar. Then in one block 50 * (2 * 10**6) = 100 * (10 ** 6) 100_000_000 SGR
  * So, 0.000001 SGR represent one cristall of sugar
  */
 contract Sugar is Initializable, ERC20Upgradeable {
